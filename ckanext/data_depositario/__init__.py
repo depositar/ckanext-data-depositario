0b33ce684637e0690d853a06ddfa4eb386419624
Sol Lee <u103133.u103135@gmail.com>
2018-07-23 14:50:51 +0800

    Update version to 6.3.0b

--- ckanext/data_depositario/__init__.py
+++ ckanext/data_depositario/__init__.py
@@ -1,6 +1,6 @@
 # this is a namespace package
 
-__version__ = '6.2.0'
+__version__ = '6.3.0b'
 
 try:
     import pkg_resources
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '6.2.0'
+version = '6.3.0b'
 
 entry_points = {
     'ckan.plugins': [
