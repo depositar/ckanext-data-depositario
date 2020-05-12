35007d342910dd571f062cbad5d526501c6c41fc
Sol Lee <u103133.u103135@gmail.com>
2020-05-12 17:24:14 +0800

    Doc: update user guide

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ rst_epilog = u'''
 .. |site_name| replace:: ``研究資料寄存所``
 
 .. _Writing documentation: http://docs.ckan.org/en/2.7/contributing/documentation.html
-.. _User guide — CKAN 2.7.4 documentation: http://docs.ckan.org/en/2.7/user-guide.html
+.. _User guide — CKAN 2.7.5 documentation: http://docs.ckan.org/en/2.7/user-guide.html
 .. _Maintainer’s guide — CKAN 2.7.5 documentation: http://docs.ckan.org/en/2.7/maintaining/index.html
 .. _String internationalization: http://docs.ckan.org/en/2.7/contributing/string-i18n.html
 '''
