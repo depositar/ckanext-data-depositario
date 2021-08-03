17a3762942d491f70d35e9719b20d82dd689a5e6
Sol Lee <u103133.u103135@gmail.com>
2021-08-03 11:52:35 +0800

    Support ckanext-scheming v2.1.0

---
+++ ckanext/data_depositario/schemas/__init__.py
@@ -0,0 +1,10 @@
+# this is a namespace package
+
+__version__ = '6.4.6b'
+
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
