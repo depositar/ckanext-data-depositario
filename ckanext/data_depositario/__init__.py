08e3f70aaf9ebc030df2d4184520ccd5da18410f
Sol Lee <u103133.u103135@gmail.com>
2023-06-28 05:54:31 +0000

    Bump version: 6.6.0b → 6.6.0

--- ckanext/data_depositario/__init__.py
+++ ckanext/data_depositario/__init__.py
@@ -1,6 +1,6 @@
 # this is a namespace package
 
-__version__ = '6.6.0b'
+__version__ = '6.6.0'
 
 try:
     import pkg_resources
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '6.6.0b'
+version = '6.6.0'
 
 entry_points = {
     'ckan.plugins': [
