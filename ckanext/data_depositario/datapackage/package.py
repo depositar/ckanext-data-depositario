298bbff8b07a35446a2a2f1a2dc71fd2924db55b
Sol Lee <u103133.u103135@gmail.com>
2026-04-17 17:16:00 +0800

    Revert "Data Package: URL of DP profile JSON"
    This reverts commit fccae686334ec9186bdf7e87acf7a3b9c00823d7.

--- ckanext/data_depositario/datapackage/package.py
+++ ckanext/data_depositario/datapackage/package.py
@@ -137,7 +137,7 @@ class DepositarCkanPackage(CkanPackage):
 
         package.custom["$schema"] = \
             "https://raw.githubusercontent.com/depositar/" \
-            "ckanext-data-depositario/dp-support/depositar-dp/1.0.0/" \
+            "ckanext-data-depositario/master/depositar-dp/1.0.0/" \
             "depositar-dp-profile.json"
 
         # Resources
